df079cfddb819938a0731a6c34534728f03e3548
officialarcode <officialarcodemusic06@gmail.com>
2025-12-15 10:42:50 +0500

    Реализованные модели. Бартова

---
+++ Interface_Bartova/Models/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Bartova.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Creat { get; set; }
+        public int IdUser { get; set; }
+        public Messages() { }
+        public Messages(string message, DateTime creat, int idUser) {
+            Message = message;
+            Creat = creat;
+            IdUser = idUser;
+        }
+    }
+}
