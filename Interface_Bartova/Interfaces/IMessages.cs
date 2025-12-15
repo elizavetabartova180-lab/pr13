28328d8aae3375b5b08bfc184466861f3c618b3e
officialarcode <officialarcodemusic06@gmail.com>
2025-12-15 10:48:42 +0500

    Реализованные интерфейсы. Бартова

---
+++ Interface_Bartova/Interfaces/IMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Interface_Bartova.Models;
+
+namespace Interface_Bartova.Interfaces
+{
+    public interface IMessages
+    {
+        void All(out List<Messages> Messages);
+        void Save(bool Update = false);
+        void Delete();
+    }
+}
---
+++ Interface_Bartova/Interfaces/IUsers.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Interface_Bartova.Models;
+
+namespace Interface_Bartova.Interfaces
+{
+    public interface IUsers
+    {
+        void All(out List<Users> Users);
+    }
+}
