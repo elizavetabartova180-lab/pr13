cb337a6b4299d9c1a8a204b1bcfb90a247e2be4b
officialarcode <officialarcodemusic06@gmail.com>
2025-12-15 11:39:42 +0500

    Реализованные классы реализующие интерфейсы.Бартова

---
+++ Interface_Bartova/Classes/MessagesContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interface_Bartova.Interfaces;
+using Interface_Bartova.Models;
+
+namespace Interface_Bartova.Classes
+{
+    public class MessagesContext : Messages , IMessages
+    {
+        public static List<Messages> AllMessages;
+        public MessagesContext() => All(out AllMessages);
+        public MessagesContext(string Messege, DateTime Creat, int IdUser): base(Messege, Creat, IdUser) { }
+        public void All(out List<Messages> Messages) =>
+            Messages = new List<Messages>();
+        public void Delete()=>
+            AllMessages.Remove(this);
+        public void Save(bool Update = false)
+        {
+            if (Update ==false)
+                AllMessages.Add(this);
+        }
+    }
+}
---
+++ Interface_Bartova/Classes/UsersContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interface_Bartova.Interfaces;
+using Interface_Bartova.Models;
+
+namespace Interface_Bartova.Classes
+{
+    public class UsersContext: Users, IUsers
+    {
+        public List<Users> AllUsers;
+        public UsersContext() =>
+            this.All(out AllUsers);
+        public void All(out List<Users> Users)
+        {
+            Users = new List<Users>();
+            AllUsers.Add(new Users( 1, "Аликина Ольга"));
+            AllUsers.Add(new Users( 2, "Бояркин Данил"));
+            AllUsers.Add(new Users( 3, "Бурмантов Владислав"));
+            AllUsers.Add(new Users( 4, "Дыджин максим"));
+            AllUsers.Add(new Users( 5, "Евдокимов Даниил"));
+            AllUsers.Add(new Users( 6, "Костюнин Никита"));
+            AllUsers.Add(new Users( 7, "Кучин Данил"));
+            AllUsers.Add(new Users( 8, "Мотырев Александр"));
+            AllUsers.Add(new Users( 9, "Пликина Ольга"));
+            AllUsers.Add(new Users( 10, "Заликина Ольга"));
+            AllUsers.Add(new Users( 11, "Аликина Марина"));
+            AllUsers.Add(new Users( 12, "Евдокимов Никита"));
+            AllUsers.Add(new Users( 13, "Мотырев Даниил"));
+
+        }
+    }
+}
